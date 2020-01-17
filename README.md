25ac804d417f67d7fc05f1114bddc9e4f8bf8b16
Artem <artyom.aweryanow@yandex.ru>
2020-01-17 23:42:32 +0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# module-films-app
---
+++ precache-manifest.5316f39bbd26d7a2c23e93ee474763fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c71562999ef55f72a8e4d0dc95bd560",
+    "url": "/module-films-app/index.html"
+  },
+  {
+    "revision": "2a431af3825044d12c58",
+    "url": "/module-films-app/static/css/main.582568b9.chunk.css"
+  },
+  {
+    "revision": "bd0bcadc760e33aba9d4",
+    "url": "/module-films-app/static/js/2.1e161527.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/module-films-app/static/js/2.1e161527.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a431af3825044d12c58",
+    "url": "/module-films-app/static/js/main.7a28b5a3.chunk.js"
+  },
+  {
+    "revision": "2a13965c4be0bc9b9369",
+    "url": "/module-films-app/static/js/runtime-main.27ef7a98.js"
+  },
+  {
+    "revision": "75fd0b9090247882c700e34fa8ed9c3e",
+    "url": "/module-films-app/static/media/HelveticaNeueCyr-Black.75fd0b90.otf"
+  },
+  {
+    "revision": "7ac1c8f0b974a943aa67859c07f86bd2",
+    "url": "/module-films-app/static/media/HelveticaNeueCyr-Bold.7ac1c8f0.otf"
+  },
+  {
+    "revision": "2127aa6d0680b144a3bd8be9debeba09",
+    "url": "/module-films-app/static/media/HelveticaNeueCyr-Light.2127aa6d.otf"
+  },
+  {
+    "revision": "c88edbac48d6408ef042ad663b7d9498",
+    "url": "/module-films-app/static/media/HelveticaNeueCyr-Medium.c88edbac.otf"
+  }
+]);
