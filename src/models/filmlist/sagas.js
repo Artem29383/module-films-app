075516f1c171eaa38d955be4c9e552c54daef2d4
Artem29383 <54779368+Artem29383@users.noreply.github.com>
2020-02-10 22:23:45 +0300

    Update sagas.js

--- src/models/filmlist/sagas.js
+++ src/models/filmlist/sagas.js
@@ -42,7 +42,6 @@ function* postFilm(action) {
 function* getFilms() {
   try {
     const payload = yield call(API.getFilms);
-    console.log(payload.data);
     const filmsSchema = new schema.Entity('filmsList');
     const filmsListSchema = [filmsSchema];
     const dataNormalized = normalize(payload.data, filmsListSchema);
