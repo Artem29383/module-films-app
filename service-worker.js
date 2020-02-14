46fbbf179229248725b41d9fe8cf45ea4a8c3dd7
Artem29383 <54779368+Artem29383@users.noreply.github.com>
2020-02-14 12:00:37 +0300

    updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/module-films-app/precache-manifest.aeb56dc38f1b541333c0dea195ed5a6f.js"
+  "/module-films-app/precache-manifest.01c3b5b0061f96a565a7fea9cf6ac371.js"
 );
 
 self.addEventListener('message', (event) => {
